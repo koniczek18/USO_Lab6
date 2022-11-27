873c2227e618e7b86763215b123a03b66b04e801
Szymon <sz.zalas@gmail.com>
2022-11-27 22:15:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+def zadanie1(active):
+    if active:
+        pass
+
+if __name__ == '__main__':
+    zadanie1(True)
